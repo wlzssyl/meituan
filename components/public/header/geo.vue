37398fe5330a5e572a7ba7770ff49f4827ac3245
wlzssyl <zhangboy7660@qq.com>
2020-10-27 20:53:01 +0800

    css文件引入，geo组件

---
+++ components/public/header/geo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location">北京市</i>
+    <nuxt-link class="changeCity" to="/changeCity">
+    切换城市</nuxt-link>
+    [廊坊 天津 香河]
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
