9882748c17f9b8a15fa5634580cf569362bbb53d
wlzssyl <zhangboy7660@qq.com>
2020-10-28 20:09:13 +0800

    header导航栏

--- components/public/header/index.vue
+++ components/public/header/index.vue
@@ -1,24 +1,28 @@
 <template>
   <el-row
   class="m-header"
-  :gutter="15">
-    <el-col :span="6"><geo/></el-col>
+  :gutter="0">
+    <el-col :span="4"><geo/></el-col>
     <el-col :span="6"><user/></el-col>
-    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :span="14"><navbar/></el-col>
   </el-row>
 </template>
 
 <script>
 import Geo from './geo.vue'
 import User from './user.vue'
+import Navbar from './nav.vue'
+
 export default {
   components: {
     Geo,
-    User
+    User,
+    Navbar
   }
 }
 </script>
 
 <style lang="scss">
   @import "@/assets/css/public/layout.scss";
+  @import "@/assets/css/public/header.scss";
 </style>
---
+++ components/public/header/nav.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="m-nav">
+    <ul class="nav">
+      <li class="list">
+        <nuxt-link to="/my">我的美团</nuxt-link>
+        <dl>
+          <dd>
+            <nuxt-link to="/1">我的订单</nuxt-link>
+          </dd>
+          <dd>
+            <nuxt-link to="/2">我的收藏</nuxt-link>
+          </dd>
+          <dd>
+            <nuxt-link to="/3">抵用券</nuxt-link>
+          </dd>
+          <dd>
+            <nuxt-link to="/4">账户设置</nuxt-link>
+          </dd>
+        </dl>
+      </li>
+      <li>
+        <nuxt-link to="/app">手机app</nuxt-link>
+      </li>
+      <li class="list bd">
+        <nuxt-link to="/center">商家中心</nuxt-link>
+      </li>
+      <li class="list site">
+        <nuxt-link to="/site">网站导航</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
