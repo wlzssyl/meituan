8075a9eecdaf55ed2f52897d74b7ab9f6444c95a
wlzssyl <zhangboy7660@qq.com>
2020-10-28 18:42:12 +0800

    header城市模块

--- components/public/header/index.vue
+++ components/public/header/index.vue
@@ -1,19 +1,22 @@
 <template>
-  <el-row class="m-header" 
+  <el-row
+  class="m-header"
   :gutter="15">
-    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :span="6"><geo/></el-col>
     <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
     <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
   </el-row>
 </template>
 
 <script>
-import Geo from './geo'
+import Geo from './geo.vue'
 export default {
-
+  components: {
+    Geo
+  }
 }
 </script>
 
 <style lang="scss">
-  @import "@/assets/css/public/layout.css";
+  @import "@/assets/css/public/layout.scss";
 </style>
