e2a9912617009a4283a343a97a0b681388cd26ae
wlzssyl <zhangboy7660@qq.com>
2020-10-29 18:57:41 +0800

    header入口文件调整

--- components/public/header/index.vue
+++ components/public/header/index.vue
@@ -1,28 +1,26 @@
 <template>
-  <el-row
-  class="m-header"
-  :gutter="0">
-    <el-col :span="4"><geo/></el-col>
-    <el-col :span="6"><user/></el-col>
-    <el-col :span="14"><navbar/></el-col>
-  </el-row>
+  <div class="m-header">
+    <el-row>
+      <el-col><top-bar/></el-col>
+    </el-row>
+    <el-row>
+      <el-col><search-bar/></el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-import Geo from './geo.vue'
-import User from './user.vue'
-import Navbar from './nav.vue'
+import topBar from './topbar.vue'
+import searchBar from './searchbar.vue'
 
 export default {
   components: {
-    Geo,
-    User,
-    Navbar
+    topBar,
+    searchBar
   }
 }
 </script>
 
-<style lang="scss">
-  @import "@/assets/css/public/layout.scss";
-  @import "@/assets/css/public/header.scss";
+<style>
+  @import "@/assets/css/public/header/index.scss";
 </style>
