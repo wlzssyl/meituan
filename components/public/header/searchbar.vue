c256b379bb9ba9b65dc5f1b3b285d437413c9734
wlzssyl <zhangboy7660@qq.com>
2020-10-30 20:04:13 +0800

    导航

--- components/public/header/searchbar.vue
+++ components/public/header/searchbar.vue
@@ -2,23 +2,23 @@
   <div class="search-panel">
     <el-row class="m-header-searchbar">
       <el-col
-      :span="3"
+      :span="2"
       class="left">
         <img src="//s0.meituan.net/bs/fe-web-meituan/fa5f0f0/img/logo.png" alt="美团">
       </el-col>
       <el-col
-      :span="15"
+      :span="18"
       class="center">
         <div class="wrapper">
           <el-input placeholder="搜索商家或地点"></el-input>
           <button class="el-button el-button-primary"><i class="el-icon-search"></i></button>
-          <dl class="hotPlace">
+          <!-- <dl class="hotPlace">
             <dt>热门搜索</dt>
             <dd>火锅</dd>
             <dd>臭鳜鱼</dd>
             <dd>罍街</dd>
             <dd>黄山烧饼</dd>
-          </dl>
+          </dl> -->
           <!-- <dl class="searchList">
             <dd>火锅</dd>
           </dl> -->
@@ -28,9 +28,29 @@
           <a href="">逍遥津</a>
           <a href="">李鸿章故居</a>
         </p>
+        <ul class="nav">
+          <li>
+            <nuxt-link to="/takeout" class="takeout">美团外卖</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/takeout" class="movie">猫眼电影</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/takeout" class="hotel">美团酒店</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/takeout" class="apartment">民宿/公寓</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/takeout" class="business">商家入驻</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/takeout" class="takeout">美团公益</nuxt-link>
+          </li>
+        </ul>
       </el-col>
       <el-col
-      :span="6"
+      :span="4"
       class="right">
         <ul class="security">
           <li>
@@ -57,6 +77,6 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+  @import "@/assets/css/public/header.scss";
 </style>
