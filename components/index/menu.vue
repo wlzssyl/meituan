16e38eae01899ecd06f5fe2cf033af2589bf0ed1
wlzssyl <zhangboy7660@qq.com>
2020-11-02 18:21:02 +0800

    首页menu

---
+++ components/index/menu.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="m-menu">
+    <dl class="nav">
+      <dt>全部分类</dt>
+      <dd
+        v-for="(value, index) in menu"
+        :key="index"
+      >
+        <i :class="value.type"/>
+        <span class="arrow">{{ value.name }}</span>
+      </dd>
+    </dl>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      menu: [
+        {
+          type: 'food',
+          name: '美食'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,12 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        mt_app
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div class="page-index">
+    <el-row>
+      <el-col :span="5">1</el-col>
+      <el-col :span="19">2</el-col>
+    </el-row>
+    <el-row :span="24">
+      3
+    </el-row>
   </div>
 </template>
 
@@ -32,42 +15,5 @@ export default {}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
