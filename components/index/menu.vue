997c069a6c09eab6ce9889d00661089c4c0efcfa
wlzssyl <zhangboy7660@qq.com>
2020-11-04 16:54:41 +0800

    首页菜单数据结构

--- components/index/menu.vue
+++ components/index/menu.vue
@@ -11,6 +11,17 @@
         <span class="arrow"></span>
       </dd>
     </dl>
+    <div class="detail">
+      <template
+        v-for="(item, index) in curdetail"
+      >
+        <h4 :key="index">{{ item.title }}</h4>
+        <span
+          v-for="v in item.child"
+          :key="v"
+        >{{ v }}</span>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -18,16 +29,44 @@
 export default {
   data () {
     return {
+      kind: '',
       menu: [
         {
           type: 'food',
-          name: '美食'
+          name: '美食',
+          child: [
+            {
+              title: '美食',
+              child: ['代金券', '甜点', '饮品', '小吃', '自助餐']
+            }
+          ]
         },
         {
           type: 'takeout',
-          name: '外卖'
+          name: '外卖',
+          child: [
+            {
+              title: '外卖',
+              child: ['美团外卖']
+            }
+          ]
+        },
+        {
+          type: 'hotel',
+          name: '酒店',
+          child: [
+            {
+              title: '酒店星级',
+              child: ['经济型', '舒适/三星', '高档/四星', '豪华/五星']
+            }
+          ]
         }
       ]
+    }
+  },
+  computed: {
+    curdetail () {
+      return []
     }
   }
 }
