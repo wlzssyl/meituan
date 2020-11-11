25d55594d810036b308d7dba49ddbd0a3bdc3e91
wlzssyl <zhangboy7660@qq.com>
2020-11-11 17:17:43 +0800

    注册表单dom

--- pages/register.vue
+++ pages/register.vue
@@ -18,12 +18,119 @@
         </span>
       </header>
     </article>
+    <el-form
+      ref="ruleForm"
+      :rules="rules"
+      :model="ruleForm"
+      label-width="100px"
+      class="demo-ruleForm"
+    >
+      <el-form-item
+        label="用户名"
+        prop="name"
+      >
+        <el-input v-model="ruleForm.name"></el-input>
+      </el-form-item>
+      <el-form-item
+        label="邮箱"
+        prop="email"
+      >
+        <el-input v-model="ruleForm.email" />
+        <el-button
+          size="mini"
+          round
+          @click="sendMsg"
+        >发送验证码</el-button>
+        <span class="status">{{ statusMsg }}</span>
+      </el-form-item>
+      <el-form-item
+        label="验证码"
+        prop="code"
+      >
+        <el-input
+          v-model="ruleForm.code"
+          maxlength="4"
+        />
+      </el-form-item>
+      <el-form-item
+        label="密码"
+        prop="pwd"
+      >
+        <el-input
+          v-model="ruleForm.pwd"
+          type="password"
+        />
+      </el-form-item>
+            <el-form-item
+              label="确认密码"
+              prop="cpwd"
+            >
+        <el-input
+          v-model="ruleForm.cpwd"
+          type="password"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          type="primary"
+          @click="register"
+        >同意以下协议并注册</el-button>
+        <div class="error">{{ error }}</div>
+      </el-form-item>
+      <el-form-item>
+        <a
+          class="f1"
+          href="https://rules-center.meituan.com/rules-detail/4"
+          target="_blank"
+        >《美团用户服务协议》</a>
+        <a
+          class="f1"
+          href="https://rules-center.meituan.com/rules-detail/2"
+          target="_blank"
+        >《美团隐私政策》</a>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
-  layout: 'blank'
+  layout: 'blank',
+  data () {
+    return {
+      statusMsg: '',
+      errot: '',
+      ruleForm: {
+        name: '',
+        email: '',
+        code: '',
+        pwd: '',
+        cpwd: ''
+      },
+      rules: {
+        name: [
+          { required: true, type: 'string', message: '请输入用户名', trigger: 'blur' }
+        ],
+        email: [
+          { required: true, type: 'email', message: '请输入邮箱', trigger: 'blur' }
+        ],
+        pwd: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ],
+        cpwd: [
+          { required: true, message: '请再次输入密码', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    sendMsg () {
+
+    },
+    register () {
+
+    }
+  }
 }
 </script>
 
