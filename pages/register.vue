e94f7f3fe6655ffa7dccd309c10d30fd7dd8464a
wlzssyl <zhangboy7660@qq.com>
2020-11-12 17:26:37 +0800

    注册表单验证

--- pages/register.vue
+++ pages/register.vue
@@ -120,7 +120,19 @@ export default {
           { required: true, message: '请输入密码', trigger: 'blur' }
         ],
         cpwd: [
-          { required: true, message: '请再次输入密码', trigger: 'blur' }
+          { required: true, message: '请再次输入密码', trigger: 'blur' },
+          {
+            validator: (rule, value, callback) => {
+              if (value === '') {
+                callback(new Error('请再次输入密码！'))
+              } else if (value != this.ruleForm.pwd) {
+                callback(new Error('两次输入的密码不一致！'))
+              } else {
+                callback()
+              }
+            },
+            trigger: 'blur'
+          }
         ]
       }
     }
